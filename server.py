1ad42defd444c854214e45c6b78526587e83bfdf
Navonil Das <navneeladas@gmail.com>
2020-07-06 22:56:22 +0530

    Handling Mouse Clicks and Position On Server

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from ctypes import windll, Structure, c_long, byref
 import socket
 import threading
 import time
+from keymap import KEYMAP
 
 PORT = 3308
 
@@ -12,8 +13,8 @@ client = None
 # client, addr = server.accept()
 
 PREV_X, PREV_Y = -1, -1
-PREV_MOUSE_LEFT = windll.user32.GetKeyState(0x01)
-PREV_MOUSE_RIGHT = windll.user32.GetKeyState(0x02)
+PREV_MOUSE_LEFT = windll.user32.GetKeyState(0x01) & 0x8000
+PREV_MOUSE_RIGHT = windll.user32.GetKeyState(0x02) & 0x8000
 
 
 class Mouse(Structure):
@@ -42,6 +43,33 @@ def threadLoop():
     global PREV_MOUSE_RIGHT
     while True:
         try:
+            mouse_left = windll.user32.GetKeyState(0x01)
+            mouse_left = mouse_left & 0x8000
+            mouse_right = windll.user32.GetKeyState(0x02)
+            mouse_right = mouse_right & 0x8000
+            x, y = getMousePos()
+
+            if mouse_left != PREV_MOUSE_LEFT:
+                PREV_MOUSE_LEFT = mouse_left
+                if mouse_left != 0:
+                    #  Left Pressed
+                    print('LP')
+                else:
+                    # Left Released
+                    print('LR')
+
+            if mouse_right != PREV_MOUSE_RIGHT:
+                PREV_MOUSE_RIGHT = mouse_right
+                if mouse_right != 0:
+                    # Right Pressed
+                    print('RP')
+                else:
+                    # Right Pressed
+                    print('RR')
+            if x != PREV_X or y != PREV_Y:
+                PREV_X, PREV_Y = x, y
+                print(x, y)
+
             pass
         except:
             pass
